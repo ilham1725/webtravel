f8d9128a82ef42b965f41d6d3c6bd353de19ed0d
ilham1725 <ilhamgadang123@gmail.com>
2021-10-11 10:07:26 +0700

    adding my files

---
+++ header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>	Travel</title>
+	    	  
+	    	  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+			  
+			  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
+			  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+			  
+			  <link rel="stylesheet" type="text/css" href="assets/css/homepage.css">
+			  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+			  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+			  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+			 <link rel="preconnect" href="https://fonts.gstatic.com">
+			 <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+			 
+			 <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+
+	<nav class="navbar navbar-expand-lg navbar-light">
+		<div class="container">
+		  <a class="navbar-brand" href="#"><i class="fa fa-map-marker fa-1x" aria-hidden="true"></i> <b>Travelio</b></a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+		    <div class="navbar-nav ml-auto">
+		      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+		      <a class="nav-item nav-link" href="#">Kontak</a>
+		      <a class="nav-item nav-link" href="#">Paket</a>
+		      <a class="nav-item nav-link" href="#">Testimonial</a>
+		      <a class="nav-link btn btn-primary tombol" href="#" style="color: white;">Masuk</a>
+		    </div>
+		  </div>
+		</div>
+	</nav>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+		require 'header.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Travelio	</title>
+			  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+			  
+			  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
+			  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+			  
+			  <link rel="stylesheet" type="text/css" href="assets/css/homepage.css">
+			  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+			  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+			  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+			  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+
+	<!-- jumbotron -->
+	<div class="jumbotron jumbotron-fluid jumbotron1">
+	  <div class="container text-center text-white mt-5">
+	    <h1>Adventure Is Out There!</h1>
+	  </div>
+	</div>
+	<!-- akhir jumbotron -->
+
+	<!-- panel search -->
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-9 panel">
+				<h3>Cari Paket Terbaikmu Disini!</h3>
+				<div class="row">
+					<div class="col-lg mt-3">
+				  		<form>
+				  			<div class="form.group">
+					  			<select class="form-control">
+					  				<option>pilih sesuai dengan kategori</option>
+					  				<option>kategori1</option>
+					  				<option>kategori2</option>
+					  			</select>
+					  		</div>
+				 	</div>
+				 	<div class="col-lg mt-3">
+				 		<div class="form.group">
+				  			<input type="text" class="form-control" type="search" placeholder="Search">
+				  		</div>
+				 	</div>
+				 	<div class="col-lg mt-3 mb-3">
+				 		<div class="form.group">
+				 		<button class="btn btn-primary tombol"> Search</button>
+				 		</form>
+				 	</div>
+				 	</div>
+				</div>
+			</div>		
+		</div>
+	</div>
+	<!-- akhir panel -->
+
+	<!-- Content -->
+
+	<div class="jumbotron">
+  <h1 class="display-4">Hello, world!</h1>
+  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </p>
+</div>
+	<!-- akhir content -->
+
+</body>
+</html>
